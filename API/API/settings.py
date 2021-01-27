02bd40e8faa82a672c6dd54028dde1b7e2dcb139
ksanbal <dev.ksanbal@gmail.com>
2021-01-28 05:11:32 +0900

    AWS RDS의 PostgreSQL로 DB 연결

--- API/API/settings.py
+++ API/API/settings.py
@@ -87,10 +87,21 @@ WSGI_APPLICATION = 'API.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'postgres',
+        'USER': 'ksanbal',
+        'PASSWORD': 'Ksanbal1021!',
+        'HOST': 'checkpm-postgresql.cwqq2majr7gz.ap-northeast-2.rds.amazonaws.com',
+        'PORT': '5432',
     }
 }
 
