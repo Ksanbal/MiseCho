d7200989d7b6b0946bf2d046c4f5a0cc3992c408
ksanbal <dev.ksanbal@gmail.com>
2021-02-02 03:15:53 +0900

    1시간마다 평균데이터를 구하는 코드 작성

--- API/checkPM/cron.py
+++ API/checkPM/cron.py
@@ -1,9 +1,34 @@
-def cron_job():
-    print('cron test')
-    # 1시간마다
-    # 디바이스별로
-    # 1시간에 해당하는 raw datas 찾아서 평균값 구하기
-    # avgdatas에 추가하기
+from . import models as mo
+from datetime import datetime, timedelta
+from django.db.models import Avg
 
-    # 회사별로 찾아서
-    # 1시간마다 전체 평균값 구하기
+
+def cron_job():
+    now_time = datetime.now()
+    target_time = now_time - timedelta(hours=1)
+    now_time = now_time.strftime('%H')
+    target_time = target_time.strftime('%H')
+
+    for i in mo.Devices.objects.all():
+        avg_pm10 = mo.RawDatas.objects.filter(
+            d_id=i,
+            time__range=[f"{target_time}:00:00", f"{target_time}:59:59"]
+        ).aggregate(Avg('pm10'))['pm10__avg']
+        avg_pm2p5 = mo.RawDatas.objects.filter(
+            d_id=i,
+            time__range=[f"{target_time}:00:00", f"{target_time}:59:59"]
+        ).aggregate(Avg('pm25'))['pm25__avg']
+
+        mo.AvgDatas.objects.create(avgpm10=avg_pm10, avgpm25=avg_pm2p5, d_id=i, c_id=i.c_id)
+
+    for i in mo.Companys.objects.all():
+        total_avg_pm10 = mo.AvgDatas.objects.filter(
+            c_id=i,
+            time__range=[f"{target_time}:50:00", f"{now_time}:10:00"]
+        ).aggregate(Avg('avgpm10'))['avgpm10__avg']
+        total_avg_pm2p5 = mo.AvgDatas.objects.filter(
+            c_id=i,
+            time__range=[f"{target_time}:50:00", f"{now_time}:10:00"]
+        ).aggregate(Avg('avgpm25'))['avgpm25__avg']
+
+        mo.TotalAvgDatas.objects.create(avgpm10=total_avg_pm10, avgpm25=total_avg_pm2p5, c_id=i)
