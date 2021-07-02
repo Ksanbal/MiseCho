4a8bc49b8ea27703db37118d135a5e8aece14526
K_Sanbal <dev.ksanbal@gmail.com>
2021-07-02 16:01:44 +0900

    비밀번호 재설정 추가

--- APP/checkpm_v2/lib/login_page.dart
+++ APP/checkpm_v2/lib/login_page.dart
@@ -141,7 +141,8 @@ class _Login2State extends State<Login2> {
 
   space_login() {
     return Container(
-      width: 300.0,
+      // width: 300.0,
+      padding: EdgeInsets.only(left: 30, right: 30),
       child: Column(
         children: <Widget>[
           Align(
@@ -183,50 +184,134 @@ class _Login2State extends State<Login2> {
   }
 
   space_btns() {
-    return Column(
-      children: <Widget>[
-        ButtonTheme(
-          minWidth: 300.0,
-          height: 50.0,
-          child: RaisedButton(
-            child: Text(
-              '로그인',
-              style: TextStyle(fontSize: 20, color: Colors.white),
+    return Padding(
+      padding: const EdgeInsets.only(left: 30, right: 30),
+      child: Column(
+        children: <Widget>[
+          ButtonTheme(
+            minWidth: 500.0,
+            height: 50.0,
+            child: RaisedButton(
+              child: Text(
+                '로그인',
+                style: TextStyle(fontSize: 20, color: Colors.white),
+              ),
+              color: Colors.lightBlue[400],
+              shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(5.0)),
+              onPressed: () {
+                signIn(id_Controller.text, pw_Controller.text);
+              },
             ),
-            color: Colors.lightBlue[400],
-            shape: RoundedRectangleBorder(
-                borderRadius: BorderRadius.circular(5.0)),
-            onPressed: () {
-              signIn(id_Controller.text, pw_Controller.text);
-            },
           ),
-        ),
-        SizedBox(
-          height: 20.0,
-        ),
-        ButtonTheme(
-          minWidth: 300.0,
-          height: 50,
-          child: RaisedButton(
-            child: Text(
-              '회원가입',
-              style: TextStyle(fontSize: 20, color: Colors.lightBlue),
+          SizedBox(height: 20.0),
+          ButtonTheme(
+            minWidth: 500.0,
+            height: 50,
+            child: RaisedButton(
+              child: Text(
+                '회원가입',
+                style: TextStyle(fontSize: 20, color: Colors.lightBlue),
+              ),
+              color: Colors.white,
+              shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(10.0)),
+              onPressed: () {
+                Navigator.push(
+                  context,
+                  PageTransition(
+                    type: PageTransitionType.rightToLeft,
+                    child: SignupPage(),
+                  ),
+                );
+              },
             ),
-            color: Colors.white,
-            shape: RoundedRectangleBorder(
-                borderRadius: BorderRadius.circular(10.0)),
-            onPressed: () {
-              Navigator.push(
-                context,
-                PageTransition(
-                  type: PageTransitionType.rightToLeft,
-                  child: SignupPage(),
-                ),
-              );
-            },
           ),
-        ),
-      ],
+          SizedBox(height: 5.0),
+          Align(
+            alignment: Alignment.centerRight,
+            child: TextButton(
+              child: Text("비밀번호 재설정"),
+              onPressed: () {
+                TextEditingController _username_ctrl = TextEditingController();
+                TextEditingController _new_pwd_ctrl = TextEditingController();
+                showDialog(
+                  context: context,
+                  builder: (context) {
+                    return AlertDialog(
+                      title: Text(
+                        "비밀번호 재설정",
+                        textAlign: TextAlign.center,
+                      ),
+                      content: Container(
+                        height: 150,
+                        child: Column(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: [
+                            TextFormField(
+                              controller: _username_ctrl,
+                              decoration: InputDecoration(
+                                labelText: '아이디',
+                              ),
+                            ),
+                            TextFormField(
+                              controller: _new_pwd_ctrl,
+                              decoration: InputDecoration(
+                                labelText: '비밀번호',
+                              ),
+                            ),
+                          ],
+                        ),
+                      ),
+                      actions: [
+                        TextButton(
+                          child: Text("Cancel"),
+                          onPressed: () => Navigator.pop(context),
+                        ),
+                        TextButton(
+                          child: Text("Ok"),
+                          onPressed: () async {
+                            var res = await http.post(
+                              "${main.apiUrl}/api/app/auth/changePwd/",
+                              headers: <String, String>{
+                                'Content-Type': "application/json",
+                              },
+                              body: jsonEncode(
+                                {
+                                  'username': _username_ctrl.text,
+                                  'new_pwd': _new_pwd_ctrl.text,
+                                },
+                              ),
+                            );
+
+                            if (res.statusCode == 200) {
+                              Navigator.pop(context);
+                              ScaffoldMessenger.of(context).showSnackBar(
+                                SnackBar(
+                                  content: Text(
+                                      "${_username_ctrl.text}님의 비밀번호가 재설정되었습니다."),
+                                ),
+                              );
+                            } else {
+                              Navigator.pop(context);
+                              ScaffoldMessenger.of(context).showSnackBar(
+                                SnackBar(
+                                  content: Text(
+                                      "${utf8.decode(res.bodyBytes)}, 비밀번호 재설정을 실패하였습니다."),
+                                ),
+                              );
+                            }
+                          },
+                        ),
+                      ],
+                    );
+                  },
+                );
+              },
+            ),
+          )
+        ],
+      ),
     );
   }
 
